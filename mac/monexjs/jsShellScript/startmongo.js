c45e10c2639d18cde92ab31483cf51354c903cb6
adeojoemmanuel <emmanuel.adeojo@yahoo.com>
2018-11-05 10:29:45 +0100

    cleaning commented codes

--- mac/monexjs/jsShellScript/startmongo.js
+++ mac/monexjs/jsShellScript/startmongo.js
@@ -17,15 +17,3 @@ function startmongo() {
 }
 
 startmongo(); 
-
-
-
-// const util = require('util');
-// const exec = util.promisify(require('child_process').exec);
-
-// async function ls() {
-//   const { stdout, stderr } = await exec('ls');
-//   console.log('stdout:', stdout);
-//   console.log('stderr:', stderr);
-// }
-// ls();
